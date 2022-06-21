83a22058abb00c61f3bff8c7424c51aef77c80a5
Lukasz Fliegel <Lukasz_Fliegel@epam.com>
2022-06-21 10:59:42 +0200

    WIP

--- WebApi.Dapper/Controllers/BankAccountsController.cs
+++ WebApi.Dapper/Controllers/BankAccountsController.cs
@@ -19,45 +19,16 @@ namespace WebApi.Dapper.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll([Required] int firmId)
         {
-            using(var connection = _dbConnectionProvider.GetConnection())
-            {
-                var bankAccounts = await connection.QueryAsync<BankAccount>(
-                    @"  SELECT * FROM BankAccounts
-                        WHERE FirmId = @firmId",
-                    new { firmId });
+            List<BankAccount> bankAccounts = null;
 
-                return Ok(bankAccounts);
-            }
+            return Ok(bankAccounts);
         }
 
         [HttpPut]
         [Route("{id}")]
         public async Task<IActionResult> Update(int id, [FromBody] BankAccountUpdate bankAccount)
         {
-            using (var connection = _dbConnectionProvider.GetConnection())
-            {
-                var numberOfRowsAffected = await connection.ExecuteAsync(
-                    @$"UPDATE BankAccounts SET 
-                            FirmId = @FirmId, 
-                            BankAccountCode = @BankAccountCode,
-                            BankAccountName = @BankAccountName,
-                            BankAccountNumber = @BankAccountNumber,
-                            IsDefault = @IsDefault,
-                            BankName = @BankName
-                        WHERE Id = @id",
-                        new
-                        {
-                            bankAccount.FirmId,
-                            bankAccount.BankAccountCode,
-                            bankAccount.BankAccountName,
-                            bankAccount.BankAccountNumber,
-                            bankAccount.IsDefault,
-                            bankAccount.BankName,
-                            id,
-                        });
-
-                return NoContent();
-            }
+            return NoContent();
         }
     }
 }
